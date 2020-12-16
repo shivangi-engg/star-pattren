767b14329be07530b29e33f95baf98d9b7cd6ead
shivangi-engg <62798764+shivangi-engg@users.noreply.github.com>
2020-12-16 13:56:54 +0530

    Add files via upload

---
+++ Patt.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+import java.io.*;
+public class Patt{
+	public static void main(String[] args) {
+		int i,j,s,r;
+
+	for (i=0;i<9 ;i++ ) {
+		for (j=1;j<i ;j++ ) {
+			System.out.print("*");
+
+		}
+		System.out.printf("%n");
+	}	
+	
+		
+	}
+
+
+}
